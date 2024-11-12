66ff8368e3bd14c9fe2c35b2cdd545487727079b
atesahmet0 <ahmetrizaates@hotmail.com>
2024-11-12 15:19:30 +0300

    Initial commit

---
+++ composeApp/src/desktopMain/kotlin/org/gcs/clonifylabs/com/main.kt
@@ -0,0 +1,13 @@
+package org.gcs.clonifylabs.com
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "KuzgunGCS",
+    ) {
+        App()
+    }
+}
